313171993e9b21049a9d22fd9199815546d97db9
Yoan Hillion <yoan.hillion@jobijoba.com>
2020-09-28 13:27:19 +0200

    add Carousel Component to details page

---
+++ src/Carousel.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+  }
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600x600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick(event) {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  }
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => {
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={(e) => this.handleIndexClick(e)}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
